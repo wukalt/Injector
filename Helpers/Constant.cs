9e9bbf451454773825bf22f6bcd6a1b78662fb7c
wuKa <wuka.sec@gmail.com>
2025-05-10 17:50:10 +0330

    use general names

--- Helpers/Constant.cs
+++ Helpers/Constant.cs
@@ -24,7 +24,7 @@ public class Constant
         get
         {
             return
-                "builder.Services.AddDbContext<LansDbContext>(options =>\r\n" +
+                "builder.Services.AddDbContext<ApplicationDbContext>(options =>\r\n" +
                 " options.UseSqlServer(builder.Configuration.GetConnectionString(\"DefaultConnection\"))\r\n" +
                 ");";
         }
@@ -37,7 +37,7 @@ public class Constant
             return
                 "," +
                 "\"ConnectionStrings\": {\r\n" +
-                "        \"DefaultConnection\": \"Server=.; Database=Lans_DB; TrustServerCertificate=true; Trusted_Connection=true\"\r\n" +
+                "        \"DefaultConnection\": \"Server=.; Database=Your_Db_Name; TrustServerCertificate=true; Trusted_Connection=true\"\r\n" +
                 "    }";
         }
     }
